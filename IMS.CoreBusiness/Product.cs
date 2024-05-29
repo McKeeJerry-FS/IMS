d429d9d90ae84851c1f306ed007d9fecf29442a7
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2024-05-29 07:04:42 -0400

    setup project to begin Product Use Cases

---
+++ IMS.CoreBusiness/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace IMS.CoreBusiness;
+
+public class Product
+{
+    public int ProductId { get; set; }       
+    public string ProductName { get; set; } = string.Empty;   
+    public int Quantity { get; set; }   
+    public double Price { get; set; }
+}
