2523a1adc5652dde63fa62e71d9c98edb845e0e5
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2024-05-31 08:50:38 -0400

    working on Adding Inventory to Products

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -12,4 +12,17 @@ public class Product
     public int Quantity { get; set; }
     [Range(0, int.MaxValue, ErrorMessage = "Price must be greater than or equal to 0")]   
     public double Price { get; set; }
+
+    public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+
+    public void AddInventory(Inventory inventory){
+        if(!this.ProductInventories.Any(x => x.Inventory != null && x.Inventory.InventoryName.Equals(inventory.InventoryName))){
+            this.ProductInventories.Add(new ProductInventory{
+                InventoryId = inventory.InventoryId,
+                Inventory = inventory,
+                InventoryQuantity = 1,
+                ProductId = this.ProductId
+            });
+        }
+    }
 }
