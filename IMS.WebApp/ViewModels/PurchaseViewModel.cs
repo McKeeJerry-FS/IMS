0a5fc25ab343fe07ea781e0848423c0641aff6d6
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-06-05 21:52:22 -0400

    continued work

---
+++ IMS.WebApp/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class PurchaseViewModel
+    {
+        [Required]
+        public string PONumber { get; set; }
+        [Required]
+        [Range(minimum:1, maximum:int.MaxValue, ErrorMessage = "You have to select an inventory.")]
+        public int InventoryId { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater than 0.")]
+
+        public int QuantityToPurchase { get; set; }
+        
+        public double InventoryPrice { get; set; }
+    }
+}
